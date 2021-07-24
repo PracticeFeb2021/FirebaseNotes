724b37f949a7d650a84af24cb091826816875c62
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2021-07-24 10:56:46 +0300

    Initial Commit

---
+++ FirebaseNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseNotes
+//
+//  Created by Oleksandr Bretsko on 24.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
